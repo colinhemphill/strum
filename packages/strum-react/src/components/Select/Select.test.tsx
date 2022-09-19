6831d8b6655027d80243cd849d522b473207b25a
Colin Hemphill <colin.hemphill@bit.ly>
2022-09-19 18:28:13 -0500

    Pass through control props for Radio component.

--- packages/strum-react/src/components/Select/Select.test.tsx
+++ packages/strum-react/src/components/Select/Select.test.tsx
@@ -9,6 +9,28 @@ describe('<Select />', () => {
     expect(screen.getByRole('combobox')).toBeVisible();
   });
 
+  it('passing value controls the input', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Select placeholder="Select an instrument" value="piano">
+        <SelectItem text="Guitar" value="guitar" />
+        <SelectItem text="Piano" value="piano" />
+      </Select>,
+    );
+
+    await user.click(screen.getByRole('combobox'));
+
+    expect(screen.getByRole('option', { name: 'Guitar' })).toHaveAttribute(
+      'aria-selected',
+      'false',
+    );
+    expect(screen.getByRole('option', { name: 'Piano' })).toHaveAttribute(
+      'aria-selected',
+      'true',
+    );
+  });
+
   it('clicking the select shows options', async () => {
     const user = userEvent.setup();
 
@@ -33,27 +55,5 @@ describe('<Select />', () => {
     expect(screen.getByText('Woodwinds')).toBeVisible();
     expect(screen.getByText('Violin')).toBeVisible();
     expect(screen.getByText('Clarinet')).toBeVisible();
-  });
-
-  it('passing value controls the input', async () => {
-    const user = userEvent.setup();
-
-    render(
-      <Select placeholder="Select an instrument" value="piano">
-        <SelectItem text="Guitar" value="guitar" />
-        <SelectItem text="Piano" value="piano" />
-      </Select>,
-    );
-
-    await user.click(screen.getByRole('combobox'));
-
-    expect(screen.getByRole('option', { name: 'Guitar' })).toHaveAttribute(
-      'aria-selected',
-      'false',
-    );
-    expect(screen.getByRole('option', { name: 'Piano' })).toHaveAttribute(
-      'aria-selected',
-      'true',
-    );
   });
 });
